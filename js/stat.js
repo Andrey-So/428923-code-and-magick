31495bcdbe57906dc13a758153b6ddc85f4a6f31
Andrey Sorokin <mail@andrey.so>
2017-11-21 20:39:15 +0600

    Избавился от констант, добавил константы :)

--- js/stat.js
+++ js/stat.js
@@ -1,24 +1,24 @@
 'use strict';
 
-const CLOUD_WIDTH = 420;
-const CLOUD_HEIGHT = 270;
-const CLOUD_X = 100;
-const CLOUD_Y = 10;
-const SHADOW_OFFSET = 10;
-const CONTENT_X = 150;
-const COLUMN_WIDTH = 40;
-const COLUMN_WITDH_OFFSET = 90;
-const HISTOGRAM_HEIGHT = 150;
-const COLOR_BLACK = '#000';
-const COLOR_WHITE = '#FFF';
-const TEXTLINE1_CONTENT = 'Ура вы победили!';
-const TEXTLINE1_X = 240;
-const TEXTLINE1_Y = 30;
-const TEXTLINE2_CONTENT = 'Список результатов:';
-const TEXTLINE2_X = 223
-const TEXTLINE2_Y = 55;
-const TEXTLINE3_CONTENT = 'Вы';
-const RGBA_RED = 'rgba(255, 0, 0, 1)';
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var SHADOW_OFFSET = 10;
+var CONTENT_X = 150;
+var COLUMN_WIDTH = 40;
+var COLUMN_WITDH_OFFSET = 90;
+var HISTOGRAM_HEIGHT = 150;
+var COLOR_BLACK = '#000';
+var COLOR_WHITE = '#FFF';
+var TEXTLINE1_CONTENT = 'Ура вы победили!';
+var TEXTLINE1_X = 240;
+var TEXTLINE1_Y = 30;
+var TEXTLINE2_CONTENT = 'Список результатов:';
+var TEXTLINE2_X = 223
+var TEXTLINE2_Y = 55;
+var TEXTLINE3_CONTENT = 'Вы';
+var RGBA_RED = 'rgba(255, 0, 0, 1)';
 
 window.renderStatistics = function (ctx, names, times) {
   var shrinkRatio = Math.max.apply(null, times) / HISTOGRAM_HEIGHT;
@@ -31,7 +31,7 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText(TEXTLINE1_CONTENT, TEXTLINE1_X, TEXTLINE1_Y);
   ctx.fillText(TEXTLINE2_CONTENT, TEXTLINE2_X, TEXTLINE2_Y);
 
-  names.forEach(function(name, i) {
+  names.forEach(function (name, i) {
     var xCoord = CONTENT_X + (i * COLUMN_WITDH_OFFSET);
     var time = times[i];
     ctx.fillStyle = COLOR_BLACK;
@@ -39,6 +39,5 @@ window.renderStatistics = function (ctx, names, times) {
     ctx.fillText(name, CONTENT_X + (i * COLUMN_WITDH_OFFSET), 250);
     ctx.fillStyle = name === TEXTLINE3_CONTENT ? RGBA_RED : 'rgba(0, 0, 255, ' + Math.random() + ')';
     ctx.fillRect(xCoord, 220, COLUMN_WIDTH, -(time / shrinkRatio));
-    }
-  )
+  });
 };
