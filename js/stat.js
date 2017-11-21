ec117d53b8cc3607c24001a9f09af8e5f4ec8ed3
Andrey Sorokin <mail@andrey.so>
2017-11-21 16:43:57 +0600

    Замечания по правкам

--- js/stat.js
+++ js/stat.js
@@ -1,29 +1,44 @@
 'use strict';
 
+const CLOUD_WIDTH = 420;
+const CLOUD_HEIGHT = 270;
+const CLOUD_X = 100;
+const CLOUD_Y = 10;
+const SHADOW_OFFSET = 10;
+const CONTENT_X = 150;
+const COLUMN_WIDTH = 40;
+const COLUMN_WITDH_OFFSET = 90;
+const HISTOGRAM_HEIGHT = 150;
+const COLOR_BLACK = '#000';
+const COLOR_WHITE = '#FFF';
+const TEXTLINE1_CONTENT = 'Ура вы победили!';
+const TEXTLINE1_X = 240;
+const TEXTLINE1_Y = 30;
+const TEXTLINE2_CONTENT = 'Список результатов:';
+const TEXTLINE2_X = 223
+const TEXTLINE2_Y = 55;
+const TEXTLINE3_CONTENT = 'Вы';
+const RGBA_RED = 'rgba(255, 0, 0, 1)';
+
 window.renderStatistics = function (ctx, names, times) {
-  var cloudWidth = 420;
-  var cloudHeight = 270;
-  var cloudX = 100;
-  var cloudY = 10;
-  var shadowOffset = 10;
-  var contentX = 150;
-  var columnWidth = 40;
-  var columnWidthOffset = 90;
-  var histogramHeight = 150;
-  var shrinkRatio = Math.max.apply(null, times) / histogramHeight;
+  var shrinkRatio = Math.max.apply(null, times) / HISTOGRAM_HEIGHT;
   ctx.font = '16px PT Mono';
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(cloudX + shadowOffset, cloudY + shadowOffset, cloudWidth, cloudHeight);
-  ctx.fillStyle = '#fff';
-  ctx.fillRect(cloudX, cloudY, cloudWidth, cloudHeight);
-  ctx.fillStyle = '#000';
-  ctx.fillText('Ура вы победили!', 240, 30);
-  ctx.fillText('Список результатов:', 223, 55);
-  for (var i = 0; i < names.length; i++) {
-    ctx.fillStyle = '#000';
-    ctx.fillText(Math.floor(times[i]), contentX + (i * columnWidthOffset), 235);
-    ctx.fillText(names[i], contentX + (i * columnWidthOffset), 250);
-    ctx.fillStyle = names[i] === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'rgba(0, 0, 255, ' + Math.random() + ')';
-    ctx.fillRect(contentX + (i * columnWidthOffset), 220, columnWidth, -(times[i] / shrinkRatio));
-  }
+  ctx.fillRect(CLOUD_X + SHADOW_OFFSET, CLOUD_Y + SHADOW_OFFSET, CLOUD_WIDTH, CLOUD_HEIGHT);
+  ctx.fillStyle = COLOR_WHITE;
+  ctx.fillRect(CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT);
+  ctx.fillStyle = COLOR_BLACK;
+  ctx.fillText(TEXTLINE1_CONTENT, TEXTLINE1_X, TEXTLINE1_Y);
+  ctx.fillText(TEXTLINE2_CONTENT, TEXTLINE2_X, TEXTLINE2_Y);
+
+  names.forEach(function(name, i) {
+    var xCoord = CONTENT_X + (i * COLUMN_WITDH_OFFSET);
+    var time = times[i];
+    ctx.fillStyle = COLOR_BLACK;
+    ctx.fillText(Math.floor(time), xCoord, 235);
+    ctx.fillText(name, CONTENT_X + (i * COLUMN_WITDH_OFFSET), 250);
+    ctx.fillStyle = name === TEXTLINE3_CONTENT ? RGBA_RED : 'rgba(0, 0, 255, ' + Math.random() + ')';
+    ctx.fillRect(xCoord, 220, COLUMN_WIDTH, -(time / shrinkRatio));
+    }
+  )
 };
