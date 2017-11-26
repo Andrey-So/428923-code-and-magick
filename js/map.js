3d19c1f92aebefec922a8431cc1a75ef79f39d3b
Andrey Sorokin <mail@andrey.so>
2017-11-26 14:32:03 +0600

    Создал map.js

---
+++ js/map.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
+var announcement = [];
+var announcementAutor = [];
+var announcementOffer = [];
+var announcementLocation = [];
+
+
+for (var i = 0; i < 8; i++) {
+  announcementOffer.push({})
+}
