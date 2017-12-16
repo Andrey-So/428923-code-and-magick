19d6cb10576d426fbed70e54e3e3a10e1b7fab54
Andrey Sorokin <mail@andrey.so>
2017-12-16 18:53:51 +0600

    Merge branch 'module5-task1' of https://github.com/Andrey-So/428923-code-and-magick into module5-task1
    # Conflicts:
    #	js/setup.js

--- js/setup.js
+++ js/setup.js
@@ -1,5 +1,14 @@
 'use strict';
+
 (function () {
+  var ENTER_KEYCODE = 13;
+  var ESC_KEYCODE = 27;
+  var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+  var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+  var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+  var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+  var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
+
   var setup = document.querySelector('.setup');
   var setupOpen = document.querySelector('.setup-open');
   var setupClose = setup.querySelector('.setup-close');
@@ -23,12 +32,12 @@
 
   var similarCharacters = [];
 
-  for (var i = 0; i < window.constants.NUMBER_OF_WIZARDS; i++) {
+  for (var i = 0; i < 4; i++) {
     similarCharacters.push({
-      name: window.constants.NAMES[Math.floor(Math.random() * window.constants.NAMES.length)],
-      surname: window.constants.SURNAMES[Math.floor(Math.random() * window.constants.SURNAMES.length)],
-      coatColor: window.constants.COAT_COLORS[Math.floor(Math.random() * window.constants.COAT_COLORS.length)],
-      eyesColor: window.constants.EYES_COLORS[Math.floor(Math.random() * window.constants.EYES_COLORS.length)]
+      name: NAMES[Math.floor(Math.random() * NAMES.length)],
+      surname: SURNAMES[Math.floor(Math.random() * SURNAMES.length)],
+      coatColor: COAT_COLORS[Math.floor(Math.random() * COAT_COLORS.length)],
+      eyesColor: EYES_COLORS[Math.floor(Math.random() * EYES_COLORS.length)]
     });
     fragment.appendChild(renderWizard(similarCharacters[i]));
   }
@@ -36,7 +45,7 @@
   setup.querySelector('.setup-similar').classList.remove('hidden');
 
   var onSetupEscPress = function (evt) {
-    if (evt.keyCode === window.constants.ESC_KEYCODE) {
+    if (evt.keyCode === ESC_KEYCODE) {
       closeSetup();
     }
   };
@@ -60,7 +69,7 @@
   });
 
   setupOpen.addEventListener('keydown', function (evt) {
-    if (evt.keyCode === window.constants.ENTER_KEYCODE) {
+    if (evt.keyCode === ENTER_KEYCODE) {
       openSetup();
     }
   });
@@ -70,20 +79,20 @@
   });
 
   setupClose.addEventListener('keydown', function (evt) {
-    if (evt.keyCode === window.constants.ENTER_KEYCODE) {
+    if (evt.keyCode === ENTER_KEYCODE) {
       closeSetup();
     }
   });
 
   wizardCoat.addEventListener('click', function () {
-    changeColor(wizardCoat, window.constants.COAT_COLORS);
+    changeColor(wizardCoat, COAT_COLORS);
   });
 
   wizardEyes.addEventListener('click', function () {
-    changeColor(wizardEyes, window.constants.EYES_COLORS);
+    changeColor(wizardEyes, EYES_COLORS);
   });
 
   fireball.addEventListener('click', function () {
-    fireball.style.backgroundColor = window.constants.FIREBALL_COLORS[Math.floor(Math.random() * window.constants.FIREBALL_COLORS.length)];
+    fireball.style.backgroundColor = FIREBALL_COLORS[Math.floor(Math.random() * FIREBALL_COLORS.length)];
   });
 })();
